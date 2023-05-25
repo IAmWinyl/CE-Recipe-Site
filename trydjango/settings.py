0d402873e9b86d3efc63e87a974398e4e1f3a894
Serra Abak <serra.abak@gmail.com>
2023-05-24 20:34:58 -0700

    Fixed allowed_host config

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -28,7 +28,7 @@ DEBUG = str(os.environ.get("DEBUG")) == "1"
 
 ALLOWED_HOSTS = []
 if not DEBUG:
-    ALLOWED_HOSTS += os.environ.get("ALLOWED_HOST")
+    ALLOWED_HOSTS += os.environ.get("DJANGO_ALLOWED_HOST")
 
 
 # Application definition
