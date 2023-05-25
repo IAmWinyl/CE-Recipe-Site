b577b9aa83f5604077fea6ffe0931b2e438222d9
Serra Abak <serra.abak@gmail.com>
2023-05-24 22:20:11 -0700

    Fixed allowed hosts config

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -27,10 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",'django-insecure-5sef3e%!&-+pm$b
 DEBUG = str(os.environ.get("DEBUG")) == "1"
 
 ENV_ALLOWED_HOST = os.environ.get('DJANGO_ALLOWED_HOST') or None
-
-ALLOWED_HOSTS = []
-if not DEBUG:
-    ALLOWED_HOSTS = [ ENV_ALLOWED_HOST ]
+ALLOWED_HOSTS = [ ENV_ALLOWED_HOST ]
 
 # Application definition
 # python manage.py makemigrations
